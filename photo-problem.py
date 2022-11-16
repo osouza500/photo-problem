3b306bf6f395be3e92d356954caecc1481c2e186
osouza <mauricio.souza002@gmail.com>
2022-11-16 17:19:13 -0200

    Criar função criar_diretório; corrigir loop for de extração de palavra-chave.

--- photo-problem.py
+++ photo-problem.py
@@ -2,16 +2,19 @@ import os
 
 # A função a seguir extrai uma palavra-chave do nome do arquivo; neste exercício,
 # as palavras-chaves estão entre subtraços.
-def extract_place(nome_arquivo):
+def extrair_palavra(nome_arquivo):
     return nome_arquivo.split("_")[1]
 
+def criar_diretorio(lugares):
+    for lugar in lugares:
+        os.mkdir(lugar)
+    
 os.chdir("Photos")
 originais = os.listdir()
-
 lugares = []
-
 for arquivos in originais:
-    palavra_chave = extract_place(arquivos)
-    lugares.append(palavra_chave)
+    palavra_chave = extrair_palavra(arquivos)
+    if palavra_chave not in lugares:
+        lugares.append(palavra_chave)
 
-print(lugares)    
+# criar_diretorio(lugares)
