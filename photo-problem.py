d52b45e9e371dcb237151c364044192e4bcd06fc
osouza <mauricio.souza002@gmail.com>
2022-11-16 15:45:39 -0200

    Criar função p/ extrair palavra-chave de nome de arquivo.

--- photo-problem.py
+++ photo-problem.py
@@ -1,5 +1,15 @@
 import os
 
-originals = os.listdir("Photos")
+def extract_place(nome_arquivo):
+    primeira_parte = nome_arquivo.find("_")
+    parcial = nome_arquivo[primeira_parte+1:]
+    segunda_parte = parcial.find("_")
+    definitivo = parcial[:segunda_parte]
+    print(definitivo)
 
-print(originals)
+extract_place("2016-11-04_Berlin_09/42/22.jpg")    
+
+# os.chdir("Photos")
+# originals = os.listdir()
+
+# print(originals)
