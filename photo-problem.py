2800beaf708c463d2cad322af6cb8d2e3dd4d790
osouza <mauricio.souza002@gmail.com>
2022-11-16 16:24:34 -0200

    Definir loop for para extrair palavra-chave e adicionar à lista 'lugares'.

--- photo-problem.py
+++ photo-problem.py
@@ -1,11 +1,17 @@
 import os
 
-os.chdir("Photos")
-originais = os.listdir()
-
 # A função a seguir extrai uma palavra-chave do nome do arquivo; neste exercício,
 # as palavras-chaves estão entre subtraços.
 def extract_place(nome_arquivo):
     return nome_arquivo.split("_")[1]
-    
-extract_place("2016-11-04_Berlin_09/42/22.jpg")    
+
+os.chdir("Photos")
+originais = os.listdir()
+
+lugares = []
+
+for arquivos in originais:
+    palavra_chave = extract_place(arquivos)
+    lugares.append(palavra_chave)
+
+print(lugares)    
