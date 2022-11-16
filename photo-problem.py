3c85ba247b59dda22b3caf0b95355f3f5578d554
osouza <mauricio.souza002@gmail.com>
2022-11-16 16:13:37 -0200

    Alterar função extract_place. Substituir método .find para .split

--- photo-problem.py
+++ photo-problem.py
@@ -1,15 +1,11 @@
 import os
 
+os.chdir("Photos")
+originais = os.listdir()
+
+# A função a seguir extrai uma palavra-chave do nome do arquivo; neste exercício,
+# as palavras-chaves estão entre subtraços.
 def extract_place(nome_arquivo):
-    primeira_parte = nome_arquivo.find("_")
-    parcial = nome_arquivo[primeira_parte+1:]
-    segunda_parte = parcial.find("_")
-    definitivo = parcial[:segunda_parte]
-    print(definitivo)
-
+    return nome_arquivo.split("_")[1]
+    
 extract_place("2016-11-04_Berlin_09/42/22.jpg")    
-
-# os.chdir("Photos")
-# originals = os.listdir()
-
-# print(originals)
