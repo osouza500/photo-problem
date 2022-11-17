f53b482e106be908a8b7feb34023bf192800bb4d
osouza <mauricio.souza002@gmail.com>
2022-11-17 11:02:32 -0200

    Formatar código de acordo com pycodestyle.

--- organizar_fotos.py
+++ organizar_fotos.py
@@ -1,19 +1,25 @@
 import os
 
-# A função a seguir extrai uma palavra-chave do nome do arquivo; neste exercício,
+
+# A função a seguir extrai uma palavra-chave
+# do nome do arquivo; neste exercício,
 # as palavras-chaves estão entre subtraços.
 def extrair_palavra(nome_arquivo):
     return nome_arquivo.split("_")[1]
 
-# A função a seguir cria um diretório de acordo com a palavra-chave extraída do
+
+# A função a seguir cria um diretório de acordo
+# com a palavra-chave extraída do
 # nome do arquivo e anexada à lista "lugares".
 def criar_diretorio(lugares):
     for lugar in lugares:
         os.mkdir(lugar)
 
-#Esta função extrai a palavra-chave, cria o diretório de acordo com ela e move os
-#arquivos para as pastas correspondentes.
-def organizar_fotos(diretorio):    
+
+# Esta função extrai a palavra-chave, cria o
+# diretório de acordo com ela e move os
+# arquivos para as pastas correspondentes.
+def organizar_fotos(diretorio):
     os.chdir(diretorio)
     originais = os.listdir()
     lugares = []
@@ -24,8 +30,8 @@ def organizar_fotos(diretorio):
     criar_diretorio(lugares)
     for arquivos in originais:
         lugar = extrair_palavra(arquivos)
-        os.rename(arquivos, 
-                 os.path.join(lugar, arquivos))
+        os.rename(arquivos, os.path.join(lugar, arquivos))
+
 
 if __name__ == "__main__":
     organizar_fotos("Photos")
